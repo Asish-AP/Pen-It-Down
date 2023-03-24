bdc88798f627100ef4eb1fc948cc53ef104f6869
Asish Panda <88313279+Asish-AP@users.noreply.github.com>
2023-03-24 22:26:33 +0530

    error

--- api/db.js
+++ api/db.js
@@ -1,8 +1,12 @@
 import mysql from "mysql";
 
-export const db = mysql.createPool({
+const db = mysql.createPool({
   host: "eu-cdbr-west-03.cleardb.net",
   user: "b3abc6fb8b22d1",
   password: "42cdff01",
   database: "heroku_e38287ae2c9872d",
 });
+db.on("error", function (err) {
+  console.log(err.code); // 'ER_BAD_DB_ERROR'
+});
+export default db;
