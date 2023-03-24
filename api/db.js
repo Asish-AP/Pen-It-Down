c6fbed964c14d56747f00458fd8e807f3fe17145
Asish Panda <88313279+Asish-AP@users.noreply.github.com>
2023-03-24 20:56:05 +0530

    db

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "#Asish@123",
-  database: "blog",
+  host: "eu-cdbr-west-03.cleardb.net",
+  user: "b3abc6fb8b22d1",
+  password: "42cdff01",
+  database: "heroku_e38287ae2c9872d",
 });
